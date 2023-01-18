33ab0241b90f6eb6f2b9235c0ce3e4beaab614e1
JeongIn <devforin@gmail.com>
2023-01-18 14:03:22 +0900

    socket test

--- socketiO_node/server.js
+++ socketiO_node/server.js
@@ -1,28 +1,24 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-// http server를 socket.io server로 upgrade한다
-var io = require('socket.io')(server);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require('socket.io')(http);
 
-// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index-room.html');
+var port = 3000;
+http.listen(port, () => {
+  console.log("listening on *:" + port);
 });
 
-// namespace /chat에 접속한다.
-var chat = io.of('/chat').on('connection', function(socket) {
-  socket.on('chat message', function(data){
-    console.log('message from client: ', data);
+io.on('connection', function (socket) {
+  console.log(socket.id, 'Connected');
 
-    var name = socket.name = data.name;
-    var room = socket.room = data.room;
-
-    // room에 join한다
-    socket.join(room);
-    // room에 join되어 있는 클라이언트에게 메시지를 전송한다
-    chat.to(room).emit('chat message', data.msg);
+  socket.emit('msg', `${socket.id} 연결 되었습니다.`);
+  
+  socket.on('msg', function (data) {
+    console.log(socket.id, data);
+    
+    socket.emit('msg', `Server : "${data}" 받았습니다.`);
   });
 });
 
-server.listen(3000, function() {
-  console.log('Socket IO server listening on port 3000');
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
